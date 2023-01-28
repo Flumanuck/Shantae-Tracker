9bb0f05fc5150ef5113e17bde1fbe550a74a8f88
Flumanuck <55293870+Flumanuck@users.noreply.github.com>
2023-01-28 13:42:55 -0500

    added lamp icon functionality

--- src/components/MerchGrid.jsx
+++ src/components/MerchGrid.jsx
@@ -1,3 +1,5 @@
+import LampImage from "./assets/lamp-symbol.png";
+
 function getThumbnail(url) {
   return `/Shantae-Tracker/${url}`;
 }
@@ -9,6 +11,11 @@ export function MerchGrid(props) {
         {props.merchItems.map((item) => (
           <div key={item.id} className="card">
             <div className="cardContent">
+              <div className="lampContainer">
+                {(item.haveState === "need" || item.haveState === "owned") && (
+                  <img src={LampImage} alt="lamp icon"></img>
+                )}
+              </div>
               <img
                 className="thumbnail"
                 src={getThumbnail(item.thumbnail)}
@@ -18,18 +25,22 @@ export function MerchGrid(props) {
                 <p className="merchName">{item.name}</p>
               </div>
               <div className="buttons">
-                <button
-                  onClick={() => props.handleYes(item.id)}
-                  className="buttonYes"
-                >
-                  ✔️
-                </button>
-                <button
-                  onClick={() => props.handleNo(item.id)}
-                  className="buttonNo"
-                >
-                  ❌
-                </button>
+                {
+                  <button
+                    onClick={() => props.handleYes(item.id)}
+                    className="buttonYes"
+                  >
+                    ✔️
+                  </button>
+                }
+                {
+                  <button
+                    onClick={() => props.handleNo(item.id)}
+                    className="buttonNo"
+                  >
+                    ❌
+                  </button>
+                }
               </div>
             </div>
           </div>
